1a5187f12f9112238cdf23b7b63aa0561aebe3c9
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-01-14 14:54:40 -0300

    first commit

---
+++ demo/src/main/java/com/davi/demo/CrudBasico02Application.java
@@ -0,0 +1,13 @@
+package com.davi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudBasico02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudBasico02Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/davi/demo/CrudBasico02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudBasico02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
